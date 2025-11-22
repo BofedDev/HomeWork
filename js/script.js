811364561d2ec82db8e1cd041029e0b1969357aa
Bogdan <bogdansklyarov7@gmail.com>
2025-11-22 17:44:11 +0200

    hw_4 complete

--- js/script.js
+++ js/script.js
@@ -1,11 +1,20 @@
 'use strict';
 
-let name = prompt('What is your name?');
 
-let question = confirm('Do you want to see the greeting?')
-if (question) {
-    alert("Hello," + " " + name + "!" + "How are you?");
-}
-else {
+let price = prompt("Enter your price");
+let discount = prompt("Enter your discount");
 
-}
+let priceTypeNumber = (+price);
+let discountTypeNumber = (+discount);
+    if(!isNaN(priceTypeNumber) && !isNaN(discountTypeNumber) &&
+        priceTypeNumber > 0 && discountTypeNumber >= 0 && discountTypeNumber < 70
+){
+        alert(`The price was ${priceTypeNumber}$, taking into account the discount, the price became: ${(priceTypeNumber - ((priceTypeNumber * discountTypeNumber) / 100))}$`);
+    }
+    else{
+        alert('input error')
+    }
+
+
+
+
