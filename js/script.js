f1fc26ea5de101b99947ad90124016db65645a94
Bogdan <bogdansklyarov7@gmail.com>
2025-11-22 18:58:26 +0200

    hw_5 complete

--- js/script.js
+++ js/script.js
@@ -1,20 +1,19 @@
 'use strict';
 
 
-let price = prompt("Enter your price");
-let discount = prompt("Enter your discount");
-
-let priceTypeNumber = (+price);
-let discountTypeNumber = (+discount);
-    if(!isNaN(priceTypeNumber) && !isNaN(discountTypeNumber) &&
-        priceTypeNumber > 0 && discountTypeNumber >= 0 && discountTypeNumber < 70
-){
-        alert(`The price was ${priceTypeNumber}$, taking into account the discount, the price became: ${(priceTypeNumber - ((priceTypeNumber * discountTypeNumber) / 100))}$`);
+let age = prompt('Enter your age');
+let ageTypeNumber = (+age);
+if(age === null || age === "") {
+    alert('Age not specified');
+}
+else if (!isNaN(ageTypeNumber) && ageTypeNumber < 18) {
+    let childWithAdult = confirm('You are under 18. Is there an adult with you who allows viewing?');
+    if (childWithAdult) {
+        alert('Access is permitted with the permission of an adult.')
+    } else {
+        alert('Access is denied')
     }
-    else{
-        alert('input error')
+}
+    else if(ageTypeNumber >= 18){
+        alert('Access is allowed. Enjoy your viewing!')
     }
-
-
-
-
