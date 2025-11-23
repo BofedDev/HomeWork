c402641bc4a72326a4c7a2b8e8db862dd0a3b98f
Bogdan <bogdansklyarov7@gmail.com>
2025-11-23 17:29:33 +0200

    hw_6 complete

--- js/script.js
+++ js/script.js
@@ -1,20 +1,18 @@
 'use strict';
 
-
-let price = prompt("Enter your price");
-let discount = prompt("Enter your discount");
-
-let priceTypeNumber = (+price);
-let discountTypeNumber = (+discount);
-    if(!isNaN(priceTypeNumber) && !isNaN(discountTypeNumber) &&
-        priceTypeNumber > 0 && discountTypeNumber >= 0 && discountTypeNumber < 70
-){
-        alert(`The price was ${priceTypeNumber}$, taking into account the discount, the price became: ${(priceTypeNumber - ((priceTypeNumber * discountTypeNumber) / 100))}$`);
-    }
-    else{
-        alert('input error')
-    }
-
-
-
+let age = Number(prompt("How old are you?"));
+if(isNaN(age)){
+    age = Number(prompt("Enter the correct age"));
+}
+let name = prompt("What is your name?");
+let userLocation = prompt("Where do you live?");
+let loveJavaScript = prompt('Do you like JavaScript?');
+let userAnswer = null;
+if(loveJavaScript === 'так' || loveJavaScript === 'Так' || loveJavaScript === 'ТАК') {
+    userAnswer = "love";
+}
+else{
+    userAnswer = "don't like it";
+}
+alert(`Hello, ${name}! You are ${age} years old, From the city of ${userLocation}. Setting to JavaScript: ${userAnswer}.`);
 
