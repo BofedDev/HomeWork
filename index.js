822bbf3c79e39e64df1949603b3e39e13792a7ec
Bogdan <bogdansklyarov7@gmail.com>
2026-02-13 00:16:01 +0200

    hw_33 complete

--- index.js
+++ index.js
@@ -1,33 +1,88 @@
 'use strict';
-import Student from './studentClass.js';
 
-let student1 = new Student('Bogdan', 'Sklyarov', 2006);
-let student2 = new Student('Vasya', 'Pirus', 2005);
-let student3 = new Student('Vitya', 'Pirat', 2004);
-console.log(student1);
-console.log('Вік:', student1.getAge());
+const user = {
+    lockProfile(){
+        console.log('До lockProfile - sealed:', Object.isSealed(this));
+        Object.seal(this);
+        console.log('Після lockProfile - sealed:', Object.isSealed(this));
+        return this;
+    },
 
-student1.present();
-student1.present();
-student1.absent();
-student1.mark(100);
-student1.mark(90);
+    lockHard(){
+        console.log('Після lockHard - frozen:', Object.isFrozen(this));
+        return Object.freeze(this);
+    },
 
-console.log('Середній бал:', student1.getAverageMark());
-console.log(student1.summary());
+    _firstName: '',
 
-console.log('----------------------------------');
-student2.present();
-student2.absent();
-student2.mark(52);
-student2.mark(42);
-console.log('Середній бал:', student2.getAverageMark());
-console.log(student2.summary());
+    _lastName: '',
 
-console.log('----------------------------------');
-student3.present();
-student3.present();
-student3.mark(70);
-student3.mark(100);
-console.log('Середній бал:', student3.getAverageMark());
-console.log(student3.summary());
+    get fullName () {
+        return this._firstName + ' ' + this._lastName;
+    },
+
+    set fullName (value) {
+        const parts = value.trim().split(' ');
+
+        if(typeof value !== 'string' || parts.length < 2) {
+            throw new Error("Повне ім'я повинне мати мінімум 2 слова!")
+        }
+
+        for (let i = 0; i < parts.length; i++) {
+            if(parts[i].length < 2) {
+                throw new Error("Ім'я та фамілія повинні мати мінімум по 2 символи!");
+            }
+        }
+
+        this._firstName = parts[0];
+        this._lastName = parts[1];
+        Object.defineProperty(this, 'createdAt', {
+            value: new Date(),
+            writable: false,
+            configurable: false,
+            enumerable: true
+        });
+
+    }
+}
+
+Object.defineProperty(user, '_firstName', { enumerable: false });
+Object.defineProperty(user, '_lastName',   { enumerable: false });
+Object.defineProperty(user, 'fullName', { configurable: false, enumerable: true });
+
+user.fullName = 'Bogdan Sklyarov';
+
+console.log('Дескриптори:', Object.getOwnPropertyDescriptors(user));
+
+user.lockProfile();
+console.log('isSealed:', Object.isSealed(user));
+
+//Error ------------
+console.log('Error start')
+user.newField = 'test';
+console.log('newField додалось?', 'newField' in user);
+
+user.createdAt = new Date('2014-12-04');
+console.log('createdAt змінився?', user.createdAt);
+
+delete user.fullName;
+console.log('fullName існує?', 'fullName' in user);
+//Error ------------
+
+// Comparison
+
+console.log('Спроба змінити _firstName після seal:');
+user._firstName = 'Petro';
+console.log('_firstName тепер:', user._firstName);
+console.log('fullName тепер:', user.fullName);
+
+console.log('lockHard');
+user.lockHard();
+
+console.log('Спроба змінити _firstName після freeze:');
+const oldName = user._firstName;
+user._firstName = 'Vasyl'; // Error
+
+console.log('Висновок');
+console.log('seal: можна змінювати існуючі властивості');
+console.log('freeze: не можна змінювати нічого.');
